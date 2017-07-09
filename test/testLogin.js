ee3cf5694b0bac24bfc366eec324fd1618dae158
Talita <tt.munchen@gmail.com>
2017-07-09 14:18:45 -0300

    testes de login

--- test/testLogin.js
+++ test/testLogin.js
@@ -1,35 +1,63 @@
 var expect = require("chai").expect;
 var sinon = require("sinon");
-var LoginDAO = require("../app/LoginDAO");
 
-var createLoginDict = function(){
-    return {
-        username: "username",
-        pass: "senha"
-    };
-}
+describe("Login tests", function(){
+    var LoginDAO = require("../app/LoginDAO");
+    var db; //db mock
 
-//sucesso
-it('Should not trhow exception', function(){
-    let reqOpt = createLoginDict();
-    LoginDAO.validate(reqOpt);  
+    //executado antes de cada teste para zerar o mock do banco
+    beforeEach(function(){
+        db = {
+            collection: function(col){return this},
+            findOne: function(dict){return this}
+        };
+    });
+
+    //Objeto dict dummy (simulando o objeto de request)
+    var createLoginDict = function(){
+        return {
+            username: "username",
+            pass: "senha"
+        };
+    }
+
+    //database
+    it('Should call database with the correct parameters', function(){
+        //espiões
+        var collectionMock = sinon.spy(db, "collection");
+        var findOneMock = sinon.spy(db, "findOne");
+
+        var dict = createLoginDict();
+
+        LoginDAO.findUserByNameAndPass(db, dict);
+
+        sinon.assert.calledWith(collectionMock, "user");
+        sinon.assert.calledWith(findOneMock, dict);
+    });
+
+    //sucesso
+    it('Should not trhow exception when validating with all required parameters', function(){
+        let reqOpt = createLoginDict();
+        LoginDAO.validate(reqOpt);  
+    });
+
+    //errors
+    it('Should trhow exception when validating user if NO username', function(){
+        let reqOpt = createLoginDict();
+        delete reqOpt.username;
+
+        expect(function(){
+            LoginDAO.validate(reqOpt);        
+        }).to.throw("No username provided");  
+    });
+
+    it('Should trhow exception when validating user if NO password', function(){
+        let reqOpt = createLoginDict();
+        delete reqOpt.pass;
+
+        expect(function(){
+            LoginDAO.validate(reqOpt);        
+        }).to.throw("No password provided");  
+    });
 });
 
-//errors
-it('Should trhow exception when validating user if no username', function(){
-    let reqOpt = createLoginDict();
-    delete reqOpt.username;
-
-    expect(function(){
-        LoginDAO.validate(reqOpt);        
-    }).to.throw("No username provided");  
-});
-
-it('Should trhow exception when validating user if no password', function(){
-    let reqOpt = createLoginDict();
-    delete reqOpt.pass;
-
-    expect(function(){
-        LoginDAO.validate(reqOpt);        
-    }).to.throw("No password provided");  
-});
